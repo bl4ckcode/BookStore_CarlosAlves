b03feb82a659962a64492c1183f1109416ba537b
Carlos Alves <carlosaugusto2005@hotmail.com>
2021-03-08 13:37:07 -0300

    Initial Commit

---
+++ BookStore_CarlosAlves/BookStore_CarlosAlvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStore_CarlosAlvesApp.swift
+//  BookStore_CarlosAlves
+//
+//  Created by Carlos Alves on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct BookStore_CarlosAlvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
